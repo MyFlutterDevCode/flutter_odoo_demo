1cf9a053d2b899399a8e012f9cd41d39ab1d21ae
excellencemichel <bnvnmmnl@gmail.com>
2021-09-10 11:16:31 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_odoo_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_odoo_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/config/app_information.dart
@@ -0,0 +1,9 @@
+class AppInformation {
+  static final name = 'flutter_odoo_demo';
+  static final title = 'Flutter_odoo_demo';
+  static final appIdAndroid = "";
+  static final appIdIos = "";
+  static final masterAdminEmail = "";
+  static final googleMapsKey = '';
+  static final googleMapsKeyURL = '';
+}
